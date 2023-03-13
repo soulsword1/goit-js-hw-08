1c1b69df65868125980b5b07a02d92b579dc9c45
Max Pieshkov <49279392+soulsword1@users.noreply.github.com>
2023-03-13 14:18:52 +0200

    added vimeo code

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,31 @@
-// import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
-// const iframeEl = document.querySelector('iframe');
+const LOCAL_WATCHED_VIDEO_KEY = 'videoplayer-current-time';
+const currentWatchedTime = localStorage.getItem(LOCAL_WATCHED_VIDEO_KEY);
 
-// const player = new Player(iframeEl);
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
 
-// // player.on('play', function() {
-// //     console.log('played the video!');
-// // });
+player.on('timeupdate', getWatchedTime);
+
+function getWatchedTime(data) {
+  const watchedTime = data.seconds;
+  localStorage.setItem(LOCAL_WATCHED_VIDEO_KEY, watchedTime);
+}
+
+player
+  .setCurrentTime(currentWatchedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
